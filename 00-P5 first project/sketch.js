0f76719a912f22e62657c41d02e44f60246efbc4
gstew07 <gstew07@outlook.com>
2024-09-10 15:19:46 -0600

    p5 code

---
+++ 00-P5 first project/sketch.js
@@ -0,0 +1,15 @@
+// Project Title
+// Your Name
+// Date
+//description
+
+//interactive program
+
+function setup() {
+  //this happens exactly once
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  circle(mouseX, mouseY, 30);
+}
