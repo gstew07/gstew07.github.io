cc3743797b8bf4a95ed25ed58fc34803b3176ef1
gstew07 <gstew07@outlook.com>
2024-09-16 15:19:57 -0600

    start of interactive project
    added basic start ups

---
+++ Interactive Scene/sketch.js
@@ -0,0 +1,18 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(800, 450);
+}
+let locationValue = 0
+let terminalNumber = 1
+
+
+function draw() {
+  background(220);
+}
