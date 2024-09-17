86982beb1f8e7820a31ce82881cd7f19331063b8
gstew07 <gstew07@outlook.com>
2024-09-17 15:21:06 -0600

    updated sketch
    added house and start code for sizing up

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -1,18 +1,33 @@
-// Project Title
-// Your Name
-// Date
+// Escape the facilty
+// Gavin Stewart
+// Sept. 17 2024
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
 
 function setup() {
   createCanvas(800, 450);
+  rectMode(CENTER)
+  noStroke()
 }
 let locationValue = 0
-let terminalNumber = 1
+let houseSize = 50
 
 
 function draw() {
   background(220);
+  if (locationValue === 0){
+    fill(50, 200, 75)
+    rect( width/2, 3 * (height/4) + 25, width, height/2 - 50);
+    fill(0,60 ,200);
+    rect( width/2, height/4 + 25 , width, height/2 + 50);
+
+  }
+  fill(181, 101, 29);
+  rect(3*width/4, 250, 1.6*houseSize, 50);
+  fill(150, 70, 25);
+  rect(580, 257, 20, 35)
+  triangle(540, 225, 600, 175, 660, 225);
+  fill(225, 225, 255);
+  square(620, 243, 14);
+
 }
