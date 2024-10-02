eafcd384419dee08690800edf57a7016ffc0d4fd
gstew07 <gstew07@outlook.com>
2024-10-02 15:15:57 -0600

    finished the project
    put flag on highest point, added panning, and set average

--- Perlin Noise Terrain Generation/sketch.js
+++ Perlin Noise Terrain Generation/sketch.js
@@ -3,11 +3,13 @@
 // Oct. 1 2024
 //using Perlin noise to generate random terrain
 
-let rectWidth = 5
+let rectWidth = 10
 let tColor = 0
 
 let tTime0 = 5; // Current "noise" time
 let tInterval = 0.02; // how fast we move down the noise graph
+
+let maxX = 0, maxY = 0
 
 
 function setup() {
@@ -19,22 +21,37 @@ function setup() {
 function draw() {
   background(220);
   generateTerrain();
-  drawFlag(width/2, height/2);
+  drawFlag(maxX, maxY);
+  tTime0 += 0.001 * (10/rectWidth)
 }
 
 function generateTerrain(){
+  let avgY = 0
+  maxY = height
+
   let tTime1 =tTime0
   for (let x = 0; x < width ; x+= rectWidth){
     let rectHeight = noise(tTime1);
-    rectHeight = map(rectHeight, 0, 1, 50, 500);
+    rectHeight = map(rectHeight, 0, 1, 100, 1000);
 
     tColor = noise(tTime1);
     tColor = map(tColor, 0, 1, 0, 125)
     fill(tColor)
     stroke(tColor)
+
     rect(x, height, rectWidth, -rectHeight)
+
     tTime1 += tInterval
+
+    if (height - rectHeight < maxY){
+      maxX = x;
+      maxY = height - rectHeight;
+    }
+    avgY += height - rectHeight
   }
+  avgY = avgY/(width/rectWidth)
+  stroke(255,0,0)
+  line(0, avgY, width, avgY)
 }
 
 function keyPressed(){
@@ -52,6 +69,6 @@ function keyPressed(){
 function drawFlag(x, y){
   fill(0);
   stroke(0);
-  line(x,y,x,y+30);
-  triangle(x, y + 10, x, y, x + 10, y + 5)
+  line(x,y,x,y-30);
+  triangle(x, y -40, x, y - 30, x + 10, y - 35)
 }
